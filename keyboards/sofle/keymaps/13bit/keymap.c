04971f2c8026b2d70ad1516a2ed592e3b893b1cb
Peter Roe <pete@13bit.me>
2023-06-14 19:45:16 -0500

    Moved left mod keys.

--- keyboards/sofle/keymaps/13bit/keymap.c
+++ keyboards/sofle/keymaps/13bit/keymap.c
@@ -14,6 +14,9 @@ enum custom_keycodes {
     KC_COLEMAK
 };
 
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
 #define TERM LCTL(KC_GRAVE)
 #define LNCH LGUI(KC_SPC)
 
@@ -34,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                    KC_LGUI,KC_LALT,KC_LCTL, MO(_LOWER), KC_ENT,   KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_DEL,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  XXXXXXX,  XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                    KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENT,   KC_SPC,  RAISE,   KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -50,18 +53,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCTR | LAlt | LGUI |LOWER | /Enter  /       \Space \  |RAISE | Term | Launch |      |
  *            |      |      |      |      |/       /         \      \ |      |      |        |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_COLEMAK] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                             KC_J,       KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                             KC_H,       KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,       XXXXXXX,     XXXXXXX, KC_K,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), TERM,    LNCH, XXXXXXX
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,  KC_7, KC_8,    KC_9,   KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                        KC_J,  KC_L, KC_U,    KC_Y,   KC_SCLN, KC_DEL,
+  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                        KC_H,  KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  XXXXXXX,     XXXXXXX, KC_K,  KC_M, KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,
+                    KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENT,      KC_SPC,  RAISE, TERM, LNCH,    XXXXXXX
 ),
 
 /* LOWER
